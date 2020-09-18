880a4f2e301d4aab6e1d277ed4bda60574e3f96b
bskordo <samat.study@gmail.com>
2020-09-18 20:25:03 +0600

    Fix static root

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY=os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG_SETTING')=='True'
 
 ALLOWED_HOSTS = []
 
@@ -124,4 +124,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
