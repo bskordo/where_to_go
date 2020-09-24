d99fa113f8d713769e340d57f9c8243e7e68c432
bskordo <samat.study@gmail.com>
2020-09-24 22:36:00 +0600

    Deleted unused functions,made some simplification

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -1,4 +1,3 @@
-#from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 from django.utils.crypto import get_random_string
 import requests
@@ -10,37 +9,30 @@ from django.core.files.base import ContentFile
 
 
 class Command(BaseCommand):
-    help = 'Create place '
+    help = 'Create place'
 
     def add_arguments(self, parser):
         parser.add_argument('site_url', type=str, help='site url with place description')
 
-
-    def get_information_from_url(self,url):
-        return requests.get(url)
-
-
     def insert_into_place_table(self, data):
-        place = Place(title=data['title'],description_short=data['description_short'],
+        place, not_created = Place.objects.get_or_create(title=data['title'],
+            description_short=data['description_short'],
             longitude=data['coordinates']['lng'],
             latitude=data['coordinates']['lat'],
             description_long=data['description_long'])
-        place.save()
-        images = data['imgs']
-        for img in images:
-            cont = self.get_information_from_url(img)
-            img_cont = Img.open(BytesIO(cont.content))
-            self.insert_into_image_table(cont.content, place)
-
+        if not_created:
+            images = data['imgs']
+            for img in images:
+                response = requests.get(img)
+                self.insert_into_image_table(response.content, place)
 
     def insert_into_image_table(self, content, place_title):
         photos = Photo(place=place_title)
         img_content = ContentFile(content)
         photos.photo.save('myphoto.jpg', img_content, save=True)
 
-
     def handle(self, *args, **kwargs):
         url = kwargs['site_url']
-        resp = self.get_information_from_url(url)
-        json_data = json.loads(resp.text)
+        response = requests.get(url)
+        json_data = json.loads(response.text)
         self.insert_into_place_table(json_data)
