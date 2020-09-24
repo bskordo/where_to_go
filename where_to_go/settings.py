c685cc6aeb7f843aa78d7bc84dc798ebe632737a
bskordo <samat.study@gmail.com>
2020-09-24 21:27:44 +0600

    Fixed pep8 mistakes

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -21,11 +21,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY=os.getenv("DJANGO_SECRET_KEY","6)n_@+zpdhnbv7tz-o)97s82*!vy56kyc5%ucr-kkhi_q_z@l%")
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "6)n_@+zpdhnbv7tz-o)97s82*!vy56kyc5%ucr-kkhi_q_z@l%")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG_SETTING')=='True'
+DEBUG = os.getenv('DEBUG_SETTING') == 'True'
 
 ALLOWED_HOSTS = []
 
