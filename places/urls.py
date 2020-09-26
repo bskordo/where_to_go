f2d1e46d12a063b835b6115495d61c80a4876510
bskordo <samat.study@gmail.com>
2020-09-26 11:30:42 +0600

    Replace id to pk

--- places/urls.py
+++ places/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from places import views
 
 urlpatterns = [
-    path(r'<id>/', views.place_details, name='place_details'),
+    path(r'<pk>/', views.place_details, name='place_details'),
     path('tinymce/', include('tinymce.urls'))
 ] 
